d16a750ba2d1aff3a3243f3803fb542dfb3ee45d
Anton Karavikou <akaravikou@solvd.com>
2022-03-16 10:55:19 +0300

    Task 1 was complete

--- resources/config.js
+++ resources/config.js
@@ -1,5 +1,6 @@
 property = {
     baseUrl: 'https://jsonplaceholder.typicode.com',
+    endPoint: '/users',
     name: 'Leanne Graham',
     username: 'Leannie',
     companyName: "Oracle"
--- test/task0.js
+++ test/task0.js
@@ -3,20 +3,20 @@ const chai = require('chai');
 const { expect } = require('chai');
 const config = require('../resources/config');
 
-let responseSchema = require('../resources/get/getResponseSchema.json');
-let postRequest = require('../resources/post/request.json');
-let postResponse = require('../resources/post/response.json');
-let requestMissingFields = require('../resources/post/requestMissingFields.json');
-let responseMissingFields = require('../resources/post/responseMissingFields.json');
-let deleteResponse = require('../resources/delete/response.json');
-let deleteRequest = require('../resources/delete/request.json');
+let responseSchema = require('../resources/users/get/getResponseSchema.json');
+let postRequest = require('../resources/users/post/request.json');
+let postResponse = require('../resources/users/post/response.json');
+let requestMissingFields = require('../resources/users/post/requestMissingFields.json');
+let responseMissingFields = require('../resources/users/post/responseMissingFields.json');
+let deleteResponse = require('../resources/users/delete/response.json');
+let deleteRequest = require('../resources/users/delete/request.json');
 
 describe("API", () => {
 
     it('Get request, check status, validate response by json schema', () => {
         chai.use(require('chai-json-schema'));
 
-        axios.get(config.baseUrl + '/users')
+        axios.get(config.baseUrl + config.endPoint)
             .then(response => {
                 chai.assert.equal(response.status, 200);
                 return response;
@@ -36,7 +36,7 @@ describe("API", () => {
         postResponse.username = config.username;
         postResponse.company.name = config.companyName;
 
-        axios.post(config.baseUrl + '/users', postRequest)
+        axios.post(config.baseUrl + config.endPoint, postRequest)
             .then((response) => {
                 expect(response.status).to.be.equal(201);
                 return response;
@@ -52,7 +52,7 @@ describe("API", () => {
         requestMissingFields.company.name = config.companyName;
         responseMissingFields.company.name = config.companyName;
 
-        axios.post(config.baseUrl + '/users', requestMissingFields)
+        axios.post(config.baseUrl + config.endPoint, requestMissingFields)
             .then(response => {
                 chai.assert.equal(response.status, 201);
                 return response;
@@ -64,7 +64,7 @@ describe("API", () => {
             });
     })
     it('Delete request, check status, validate response', () => {
-        axios.delete(config.baseUrl + '/users/1', deleteRequest)
+        axios.delete(config.baseUrl + config.endPoint + '/1', deleteRequest)
             .then(response => {
                 expect(response.status).to.equal(200);
                 return response;
